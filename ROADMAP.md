3354b1657cb07c423db1876e224c25f9b76dffef
IT Mongoose <webdevmongoose@gmail.com>
2021-02-08 18:51:43 +0100

    initial commit: typing single characters in/removing with backspace, newlines with enter

---
+++ ROADMAP.md
@@ -0,0 +1,19 @@
+# Features to implement:
+- [v] typing single characters in
+- [v] removing single characters with <Backspace>
+- [v] adding new lines with <Enter>
+- [ ] show cursor
+- [ ] move cursor around
+- [ ] delete current line with a keyboard shortcut
+- [ ] multiple cursors
+- [ ] pasting content from buffer
+- [ ] text selection and removing selected text
+- [ ] text selection and copying to buffer
+- [ ] pasting from buffer history
+- [ ] multiple selections
+
+# Non-functional requirements:
+
+- [ ] code organization
+- [ ] unit tests
+- [ ] e2e / integration tests
---
+++ packages/pico-editor-browser/src/index.ts
@@ -0,0 +1,28 @@
+function component() {
+    const element = document.createElement('pre');
+    element.innerHTML = '&lt;start typing&gt;';
+    return element;
+}
+
+let firstCharacter = true;
+const el = component();
+document.body.appendChild(el);
+document.body.addEventListener('keypress', (e) => {
+    console.log("Keypressed", e.key, e.code);
+    if (firstCharacter) {
+        el.innerText = '';
+        firstCharacter = false;
+    }
+    if (e.key === 'Enter') {
+        el.innerText += '\n';
+        return;
+    }
+    el.innerText += e.key;
+});
+document.body.addEventListener('keydown', (e) => {
+    console.log("Key down", e.key, e.code);
+    if (e.key === 'Backspace') {
+        el.innerText = el.innerText.slice(0, -1);
+    }
+    // el.innerText += e.key;
+});
